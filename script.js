969994097b5db5f98d6e05e0408df189652b5a25
satwikbolar <satwikvinodbolar@gmail.com>
2022-02-03 15:54:34 +0530

    made changes to the repo-required output obtained

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
+function simpleinterest()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal");
+    var r = document.getElementById("myRange");
+    var t= document.getElementById("years");
+    var interest=((r.value)*(p.value)*(t.value))/100;
+    var out1=document.getElementById("outer1");
+    out1.innerHTML="If you deposit "+"<span class='highlight'>"+p.value+ "</span>"+","+"<br>"+"at an interest rate of "+"<span class='highlight'>"+r.value+"%"+ "</span>"+"."+"<br>"+"You will receive an amount of "+"<span class='highlight'>"+interest+ "</span>"+","+"<br>"+"in the year "+"<span class='highlight'>"+(parseInt(new Date().getFullYear())+parseInt(t.value))+ "</span>";
+    
+    
     
 }
-        
+function rangeslider(){
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value+"%";
+
+    slider.oninput = function() {
+        output.innerHTML = this.value+"%";
+    }
+    
+}
+    
