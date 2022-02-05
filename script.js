4d69e40cbdb716688604b9a97de6ce6d7d66ca65
satwikbolar <satwikvinodbolar@gmail.com>
2022-02-05 11:03:25 +0530

    satisfied all requirements

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ function simpleinterest()
     var r = document.getElementById("myRange");
     var t= document.getElementById("years");
     var interest=((r.value)*(p.value)*(t.value))/100;
-    var out1=document.getElementById("outer1");
-    out1.innerHTML="If you deposit "+"<span class='highlight'>"+p.value+ "</span>"+","+"<br>"+"at an interest rate of "+"<span class='highlight'>"+r.value+"%"+ "</span>"+"."+"<br>"+"You will receive an amount of "+"<span class='highlight'>"+interest+ "</span>"+","+"<br>"+"in the year "+"<span class='highlight'>"+(parseInt(new Date().getFullYear())+parseInt(t.value))+ "</span>";
+    if (p.value<=0){
+        alert("Please Enter a positive value!");
+        p.focus();
+        //return false;
+    }
+    else{
+        var out1=document.getElementById("outer1");
+        out1.innerHTML="If you deposit "+"<span class='highlight'>"+p.value+ "</span>"+","+"<br>"+"at an interest rate of "+"<span class='highlight'>"+r.value+"%"+ "</span>"+"."+"<br>"+"You will receive an amount of "+"<span class='highlight'>"+interest+ "</span>"+","+"<br>"+"in the year "+"<span class='highlight'>"+(parseInt(new Date().getFullYear())+parseInt(t.value))+ "</span>";
+    }
     
     
     
@@ -20,4 +27,4 @@ function rangeslider(){
     }
     
 }
-    
+ 
